4b66cfa9c239ebbba5203311fc9d58725c9b31d1
Luca Ribeiro lopes <lucaribeiro1242@gmail.com>
2025-07-22 11:26:41 -0300

    feat: Adicionado captura de erros

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
-import express from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import router from "./routes";
+import 'express-async-errors'
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction ) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message,
+        })
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error',
+    });
+});
 
 
 app.listen(port, () => {
